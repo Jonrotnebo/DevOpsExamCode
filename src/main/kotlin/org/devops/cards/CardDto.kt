eb28b4155669c011466088b917eefe5113ebd3ec
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-11-15 16:18:35 +0100

    Removed Cards ID.

--- src/main/kotlin/org/devops/cards/CardDto.kt
+++ src/main/kotlin/org/devops/cards/CardDto.kt
@@ -5,6 +5,5 @@ class CardDto(
         var cardId : String? = null,
         var name : String? = null,
         var description: String? = null,
-        var rarity: Rarity? = null,
-        var imageId: String? = null
+        var rarity: Rarity? = null
 )
