721ebedf2ebc7f40b7cc134be136587a3b8bc6ea
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-11-19 23:31:17 +0100

    Now added a working get method, time to test

--- src/main/kotlin/org/devops/RestAPICards.kt
+++ src/main/kotlin/org/devops/RestAPICards.kt
@@ -1,10 +1,12 @@
 package org.devops
 
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.devops.cards.GameDto
 import org.devops.db.CardService
+import org.devops.db.Cards
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
@@ -12,34 +14,36 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
-@Api(value = "/api/cards", description = "Operation on the cards existing in the game")
-@RequestMapping(path = ["/api/cards"])
+@Api(value = "/cards", description = "Operation on the cards existing in the game")
+@RequestMapping(path = ["/cards"])
 @RestController
-class RestAPICards() {
-    companion object{
-        const val LATEST = "v1_000"
-    }
-    var cardsCreated = 0
+class RestAPICards(
+        @Autowired
+        private val meterRegistry: MeterRegistry
 
-    val logger = LoggerFactory.getLogger(RestAPICards::class.java.name)
+) {
 
-    @Autowired
-    private lateinit var meterRegistry: MeterRegistry
+    private val logger = LoggerFactory.getLogger(RestAPICards::class.java.name)
+
+    private val createCounter = Counter.builder("count.create").description("Cards created").register(meterRegistry)
+    private val getCounter = Counter.builder("count.get").description("get calls").register(meterRegistry)
+
+
+    //@Autowired
+    //private lateinit var meterRegistry: MeterRegistry
 
     @Autowired
     private lateinit var cardService: CardService
 
 
     @ApiOperation("Return info on all cards in the game")
-    @GetMapping(
-            path = ["/collection"],
-            produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
-    fun getLatest() : ResponseEntity<Void> {
+    @GetMapping(path = ["/collection"])
+    fun ListCards() : ResponseEntity<List<Cards>> {
 
         val collection =  cardService.getAll()
+        getCounter.increment()
 
-        return ResponseEntity.status(200).build()
+        return ResponseEntity.status(200).body(collection.toList())
     }
 /*
     @ApiOperation("Old-version endpoints. Will automatically redirect to most recent version")
@@ -60,7 +64,8 @@ class RestAPICards() {
 
     @ApiOperation("Adding own Cards to a List")
     @PostMapping(path= ["/collection"], consumes = ["application/json"], produces = ["application/json"])
-    fun createCard(@RequestBody dto: GameDto) : ResponseEntity<Void>{
+    fun createCard(@RequestBody dto: GameDto) : ResponseEntity<Cards>{
+        logger.debug("Created monster, in function: {}", createCounter.count())
         if (dto.id == null || dto.name == null) {
             logger.error("create user, id or name is null.")
             return ResponseEntity.status(400).build()
@@ -69,17 +74,19 @@ class RestAPICards() {
         val ok = cardService.createNewCard(dto.id!!, dto.name!!)
 
         meterRegistry.counter("txcount", "name", dto.name).increment();
-        meterRegistry.gauge("numberOfUsers", cardsCreated)
 
         if (!ok){
             logger.error("Creation of user failed.")
             return ResponseEntity.status(400).build()
         } else{
-            ++cardsCreated
-            logger.info("Create Card Sucsess")
+
+            createCounter.increment()
+            logger.debug("Created monster after create: {}", createCounter.count())
+            logger.info("Create Card Sucsess.")
             return ResponseEntity.status(201).build()
         }
 
-
     }
+
+
 }
